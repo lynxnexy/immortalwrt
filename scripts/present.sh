89830a7d4e934efa1f11007f8b6a93d7a26a73fe
lynxnexy <81577496+lynxnexy@users.noreply.github.com>
2022-06-10 00:06:42 +0700

    Update present.sh

--- scripts/present.sh
+++ scripts/present.sh
@@ -2,7 +2,8 @@
 #
 # Copyright 2022 by lynxnexy <https://github.com/lynxnexy/immortalwrt>
 # 
-cat << EOF > files/usr/lib/lua/luci/view/openclash/editor.htm
+
+cat << EOF > feeds/luci/applications/luci-app-openclash/luasrc/view/openclash/editor.htm
 <%+header%>
 <div class="cbi-map">
 <iframe id="editor" style="width: 100%; min-height: 100vh; border: none; border-radius: 2px;"></iframe>
@@ -13,4 +14,4 @@ document.getElementById("editor").src = "http://" + window.location.hostname + "
 <%+footer%>
 EOF
 
-sed -i '87 i\	entry({"admin", "services", "openclash", "editor"}, template("openclash/editor"),_("Config Editor"), 90).leaf = true' files/usr/lib/lua/luci/controller/openclash.lua
+sed -i '87 i\	entry({"admin", "services", "openclash", "editor"}, template("openclash/editor"),_("Config Editor"), 90).leaf = true' feeds/luci/applications/luci-app-openclash/luasrc/controller/openclash.lua
